cfc67c3dc28b5f17826b549d52d76a09fca03ce6
Antnamve <antnamve@gmail.com>
2024-02-25 07:03:03 +0300

    feat: added property model and connected with frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Property.create!({
+  name: 'Sample Property',
+  description: 'Sample Property Description',
+  headline: 'Sample Property Headline',
+  address_1: 'Sample Property Address 1',
+  address_2: 'Sample Property Address 2',
+  city: 'Sample Property City',
+  state: 'Sample Property State',
+  country: 'Sample Property Country'
+})
